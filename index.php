cf5c6996ebe32f819d575627b344c303ab840b55
squish3993 <jdlewis@wesleyan.edu>
2017-09-11 22:54:46 -0400

    Added HTML for image and about me

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head> 
+		<title>Jacob Lewis</title>
+	</head>
+	
+	<body>
+		<div class="nameAndImage">
+			<h1>Jacob Lewis</h1>
+			<img src="/images/Profpic.jpg" alt="Jacob Lewis"/>
+		</div>
+		<div class="aboutMe">
+			<h2>About Me</h2>
+			<p>
+				Hello, my name is Jacob Lewis, but I generally go by the nane 'Jack'.<br/> I was born in New York City and I currently live in Somerville, Ma. </p>
+	</body>
+</html>
+
+
