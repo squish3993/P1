83aef079bb6f358cd261574a30785f8e568e5523
squish3993 <jdlewis@wesleyan.edu>
2017-09-13 11:52:18 -0400

    Cleaning up code and adding comments

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
-<?php require ('randquote.php'); ?>
+<?php require ('randquote.php'); ?>  <!--links to PHP logic code -->
 <!doctype html>
 <html>
 	<head> 
 		<title>Jacob Lewis</title>
+
+		<!--Links to Bootsrap and stylesheet -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		
 		<link rel="stylesheet" href= "p1Style.css"> 
+
 	</head>
 	
 	<body>
-		<div class="container">
-			<div class="name">
-				<h1>Jacob Lewis</h1>
-			</div>
-
-			<div class="image">
-				<img class ="resize" src="/Images/ProfPic.jpg" alt="Jacob Lewis"/>
-			</div>
+		<div class="container"> <!-- Creates a container class for the whole body -->
+			<h1>Jacob Lewis</h1> 
+			
+			<img src="/Images/ProfPic.jpg" alt="Jacob Lewis"/> <!--links to picture -->
 			
 			<h2>About Me</h2>
-			<div class="aboutMe">
 				<p>
 					Hello, my name is Jacob Lewis, but I generally go by the name 'Jack'.
 						
@@ -29,14 +28,15 @@
 						
 					I took a few introductory programming classes while in college, but recently have become more interested in Web Development, particularly back-end web development. 
 				 </p>
-			</div>
 
 			<h3>Random Quote</h3>
-				<p class="quote">
+			
+			<p class="quote"> <!--creates class for PHP generated random quote and calls PHP function -->
 				<?php 
 					echo getQuote();
 				?> 
 			</p>
+		
 		</div>
 	</body>
 </html>
--- randquote.php
+++ randquote.php
@@ -1,9 +1,16 @@
 <?php
 
-function getQuote() {
+//Creates a function that generates a random quote that will be called in the HTML file 
+function getQuote() { 
+
+	//Generate array with quotes
 	$quotes = ["Why so serious? -The Joker", "Me, I'm dishonest, and you can always trust a dishonest man to be dishonest. Honestly, it's the honest ones you have to watch out for... -Jack Sparrow", "Isn't it funny how day by day nothing changes, but when we look back everythng is different. -C.S Lewis"];
 
+	//generate a random number between 0 and the size of our array (minus 1 because arrays start at 0)
 	$num = rand(0, (count($quotes)-1));
+
+	//takes the random number and uses it to call a quote from the array. Since the number is random, the quote is random. 
 	$randQuote = $quotes[$num];
+	
 	return $randQuote;
 }
