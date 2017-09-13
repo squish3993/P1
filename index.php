f4200f9f0b86c31cf358e3b4548e23b5270e084d
squish3993 <jdlewis@wesleyan.edu>
2017-09-13 11:30:52 -0400

    final changes to format

--- index.php
+++ index.php
@@ -9,33 +9,35 @@
 	</head>
 	
 	<body>
-		<div class="name container text-center">
-			<h1>Jacob Lewis</h1>
-		</div>
+		<div class="container">
+			<div class="name">
+				<h1>Jacob Lewis</h1>
+			</div>
 
-		<div class="image container">
-			<img class ="resize" src="/Images/ProfPic.jpg" alt="Jacob Lewis"/>
-		</div>
-		
-		<h2 class="text-center">About Me</h2>
-		<div class="aboutMe container text-center">
-			<p>
-				Hello, my name is Jacob Lewis, but I generally go by the nane 'Jack'.
-					
-				I was born in New York City, and I went to college at Wesleyan University where I majored in Physics and Music. 
-					
-				I currently live in Somerville, Ma, where I play guitar in a local alternative rock band,'Jakals', who play in Bosotn and the surrounding neighborhoods. During the day, I work at Harvard University for their MTS department as an A/V Tech.
-					
-				I took a few introductory programming classes while in college, but recently have become more interested in Web Development, particularly back-end web development. 
-			 </p>
-		</div>
+			<div class="image">
+				<img class ="resize" src="/Images/ProfPic.jpg" alt="Jacob Lewis"/>
+			</div>
+			
+			<h2>About Me</h2>
+			<div class="aboutMe">
+				<p>
+					Hello, my name is Jacob Lewis, but I generally go by the nane 'Jack'.
+						
+					I was born in New York City, and I went to college at Wesleyan University where I majored in Physics and Music. 
+						
+					I currently live in Somerville, Ma, where I play guitar in a local alternative rock band,'Jakals', who play in Bosotn and the surrounding neighborhoods. During the day, I work at Harvard University for their MTS department as an A/V Tech.
+						
+					I took a few introductory programming classes while in college, but recently have become more interested in Web Development, particularly back-end web development. 
+				 </p>
+			</div>
 
-		<h3 class="text-center">Random Quote</h3>
-			<p class ="quote text-center">
-			<?php 
-				echo getQuote();
-			?> 
-		</p>
+			<h3>Random Quote</h3>
+				<p class="quote">
+				<?php 
+					echo getQuote();
+				?> 
+			</p>
+		</div>
 	</body>
 </html>
 
