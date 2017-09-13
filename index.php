12974250c45f9b0904f9933741ae68be8088dfcc
squish3993 <jdlewis@wesleyan.edu>
2017-09-13 16:53:42 -0400

    Fixed one last typo

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 						
 					I was born in New York City, and I went to college at Wesleyan University where I majored in Physics and Music. 
 						
-					I currently live in Somerville, Ma, where I play guitar in a local alternative rock band, 'Jakals', who play in Bosotn and the surrounding neighborhoods. During the day, I work at Harvard University for their MTS department as an A/V Tech.
+					I currently live in Somerville, Ma, where I play guitar in a local alternative rock band, 'Jakals', who play in Boston and the surrounding neighborhoods. During the day, I work at Harvard University for their MTS department as an A/V Tech.
 						
 					I took a few introductory programming classes while in college, but recently have become more interested in Web Development, particularly back-end web development. 
 				 </p>
